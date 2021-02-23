7d84a5199e6e4635469d6ae33a9205447545852a
Gina Häußge <gina@octoprint.org>
2021-02-23 14:29:50 +0100

    👷 Add build pipeline

--- setup.py
+++ setup.py
@@ -90,6 +90,11 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
     additional_packages=plugin_additional_packages,
     ignored_packages=plugin_ignored_packages,
     additional_data=plugin_additional_data,
+    extra_requires={
+        "develop": [  # pre-commit
+            "pre-commit",
+        ]
+    },
 )
 
 if len(additional_setup_parameters):
